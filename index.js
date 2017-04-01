e61a99b38fae2c5935c410c17067ae384bc2da16
Matthias Kern <matthias_kern@posteo.net>
2017-04-01 19:34:55 +0100

    add direct youtube link parsing

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
 const open = require('open-pip')
+const ytdl = require('ytdl-core')
 const ora = require('ora')
 const getStdin = require('get-stdin')
 const spinner = ora().start()
 
 const help = () => spinner.info('Usage: open-pip <path | youtube-url | twitch-url | other-video-url>')
+
+const YOUTUBE_HOST = new RegExp('www.youtube')
 
 getStdin()
 .then(stdin => {
@@ -17,7 +20,21 @@ getStdin()
   }
   return input
 })
-.then(input => open(input))
+.then(input => {
+  if (YOUTUBE_HOST.test(input)) {
+    spinner.stopAndPersist({symbol: '🚀', text: 'Found youtube link'})
+    ytdl.getInfo(input, { debug: true }, (err, info) => {
+      if (err) {
+        open(input)
+        return
+      }
+
+      open(info.formats[0].url)
+    })
+  } else {
+    open(input)
+  }
+})
 .then(() => {
   spinner.stopAndPersist({ symbol: '🌟', text: 'Running!' })
 })
